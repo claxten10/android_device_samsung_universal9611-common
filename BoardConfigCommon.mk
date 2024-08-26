aee00c67be60c0f001a7e41a79302b60fdd9fdb9
Christopher Ferris <cferris@google.com>
2024-08-26 23:44:15 +0100

    universal9611: Enable 32 bit building, but only 64 bit zygote.
    Bug: 229787331
    Test: Build/Boot in progress.
    Change-Id: I4686b567fd96a6cd7c3f6e269f0424811c0b3d6a

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -35,6 +35,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+# Enable 64-bit for non-zygote.
+ZYGOTE_FORCE_64 := true
+
 ## Audio
 BOARD_LOW_LATENCY_CAPTURE_DURATION := 20
 

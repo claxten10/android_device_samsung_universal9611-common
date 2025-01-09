9efb298e8109b9030672edc820ebb3174a0c977c
claxten10 <claxten10@gmail.com>
2025-01-09 14:26:58 +0000

    universal9611: Drop 32-bit display blobs
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- common.mk
+++ common.mk
@@ -91,15 +91,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.mapper@2.0-impl-2.1
+    android.hardware.graphics.mapper@2.0-impl-2.1:64
 
 PRODUCT_PACKAGES += \
     android.hardware.composer.hwc3-service.slsi \
-    gralloc.exynos9611 \
+    gralloc.exynos9611:64 \
     libion
 
 PRODUCT_PACKAGES += \
-    libGrallocWrapper
+    libGrallocWrapper:64
 
 # DRM
 PRODUCT_PACKAGES += \
